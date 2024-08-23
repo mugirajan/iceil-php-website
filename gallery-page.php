9ff4f1bb1a28f0eb2cf5db1c1f4dbbecf0043fe0
mugilavathi <you@example.com>
2024-08-23 13:21:44 +0530

    thumbnail images

--- gallery-page.php
+++ gallery-page.php
@@ -32,7 +32,7 @@
                 <div class="cards-of-blog-like">
                         <a target="_blank" href="https://www.youtube.com/watch?v=mOnSgUUKhLU&list=PL2wd3eKmgLLO7hqb36gL3jt5_t8Fz1O1X&index=1&t=7s" class="card-of-blog">
                             <div class="card-hero card-partner-height">
-                                <img alt="our project"  src="images/project-page/kalaignar library.jpg" width="288" />
+                                <img alt="our project"  src="images/youtube/thumb1.jpg" width="288" />
                             </div>
                             <div class="card-header-blog">
                                 <h3>Iceil Introduction</h3>
@@ -40,7 +40,7 @@
                         </a>
                         <a target="_blank" href="https://www.youtube.com/watch?v=BWX7K0dQ2A8&list=PL2wd3eKmgLLO7hqb36gL3jt5_t8Fz1O1X&index=2" class="card-of-blog">
                             <div class="card-hero card-partner-height">
-                                <img alt="our project"  src="images/project-page/kalaignar library.jpg" width="288" />
+                                <img alt="our project"  src="images/youtube/thumb2.jpg" width="288" />
                             </div>
                             <div class="card-header-blog">
                                 <h3>Tensile Strength Demo</h3>
@@ -48,7 +48,7 @@
                         </a>
                         <a target="_blank" href="https://www.youtube.com/watch?v=Kb7bc5a3DLw&list=PL2wd3eKmgLLO7hqb36gL3jt5_t8Fz1O1X&index=3" class="card-of-blog">
                             <div class="card-hero card-partner-height">
-                                <img alt="our project"  src="images/project-page/kauvery hospital.jpg" width="288" />
+                                <img alt="our project"  src="images/youtube/thumb3.jpg" width="288" />
                             </div>
                             <div class="card-header-blog">
                                 <h3>Fire Retardancy Demo</h3>
@@ -56,7 +56,7 @@
                         </a>
                         <a target="_blank" href="https://www.youtube.com/watch?v=QU7oaowAjL0&list=PL2wd3eKmgLLO7hqb36gL3jt5_t8Fz1O1X&index=4" class="card-of-blog">
                             <div class="card-hero card-partner-height">
-                                <img alt="our project"  src="images/project-page/Sun Beam School.jpg" width="288" />
+                                <img alt="our project"  src="images/youtube/thumb5.jpg" width="288" />
                             </div>
                             <div class="card-header-blog">
                                 <h3>6th Avenue Resto bar  - Chennai</h3>
@@ -64,23 +64,23 @@
                         </a>
                         <a target="_blank" href="https://www.youtube.com/watch?v=9RB7JrOXmdY&list=PL2wd3eKmgLLO7hqb36gL3jt5_t8Fz1O1X&index=5" class="card-of-blog">
                             <div class="card-hero card-partner-height">
-                                <img alt="our project"  src="images/project-page/DLF DownTown.jpg" width="288" />
+                                <img alt="our project"  src="images/youtube/thumb6.jpg" width="288" />
                             </div>
                                 <div class="card-header-blog">
                                     <h3>Detailing Mafia - Chennai</h3>
                                 </div>
                         </a>
-                        <a target="_blank" href="https://www.youtube.com/watch?v=qfvWO1fxgIA&list=PL2wd3eKmgLLO7hqb36gL3jt5_t8Fz1O1X&index=6" class="card-of-blog">
+                        <a target="_blank" href="https://www.youtube.com/watch?v=jSvYca5-B40&list=PL2wd3eKmgLLO7hqb36gL3jt5_t8Fz1O1X&index=7" class="card-of-blog">
                             <div class="card-hero card-partner-height">
-                                <img alt="our project"  src="images/project-page/Trimble.jpg" width="288" />
+                                <img alt="our project"  src="images/youtube/thumb9.jpg" width="288" />
                             </div>
                             <div class="card-header-blog">
                                 <h3>Vijay Park Mall - Chennai</h3>
                             </div>
                         </a>
-                        <a target="_blank" href="https://www.youtube.com/watch?v=jSvYca5-B40&list=PL2wd3eKmgLLO7hqb36gL3jt5_t8Fz1O1X&index=7" class="card-of-blog">
+                        <a target="_blank" href="https://www.youtube.com/watch?v=qfvWO1fxgIA&list=PL2wd3eKmgLLO7hqb36gL3jt5_t8Fz1O1X&index=6" class="card-of-blog">
                             <div class="card-hero card-partner-height">
-                                <img alt="our project"  src="images/project-page/GRT Radisson Blu.jpeg" width="288" />
+                                <img alt="our project"  src="images/youtube/thumb8.jpg" width="288" />
                             </div>
                             <div class="card-header-blog">
                                 <h3>Balaji Dental Clinic - Chennai</h3>
@@ -88,10 +88,10 @@
                         </a>
                         <a target="_blank" href="https://www.youtube.com/watch?v=_CbA59z6cz8&list=PL2wd3eKmgLLO7hqb36gL3jt5_t8Fz1O1X&index=8" class="card-of-blog">
                             <div class="card-hero card-partner-height">
-                                <img alt="our project"  src="images/project-page/Maharishi Vidya Mandir School.jpg" width="288" />
+                                <img alt="our project"  src="images/youtube/thumb7.jpg" width="288" />
                             </div>
                             <div class="card-header-blog">
-                                <h3>8. Power consumption Demo</h3>
+                                <h3>Power consumption Demo</h3>
                             </div>
                         </a>
                 </div>
