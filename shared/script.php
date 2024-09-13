b141a7329bc38c72c5acbe86d94264b291706817
mugilavathi <you@example.com>
2024-09-13 19:55:15 +0530

    loading

--- shared/script.php
+++ shared/script.php
@@ -5,25 +5,25 @@
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
-    <script type='text/javascript' src='js/jquery/jquery-migrate.min.js'></script>
-    <script type='text/javascript' src='plugins/revslider/public/assets/js/jquery.themepunch.tools.min.js'></script>
+    <script type='text/javascript' src='js/jquery/jquery-migrate.min.js' async></script>
+    <script type='text/javascript' src='plugins/revslider/public/assets/js/jquery.themepunch.tools.min.js' defer></script>
     <script type='text/javascript'
-        src='plugins/revslider/public/assets/js/jquery.themepunch.revolution.min.js'></script>
+        src='plugins/revslider/public/assets/js/jquery.themepunch.revolution.min.js' async></script>
     <script type="text/javascript"
-        src="plugins/revslider/public/assets/js/extensions/revolution.extension.slideanims.min.js"></script>
+        src="plugins/revslider/public/assets/js/extensions/revolution.extension.slideanims.min.js" async></script>
     <script type="text/javascript"
-        src="plugins/revslider/public/assets/js/extensions/revolution.extension.layeranimation.min.js"></script>
+        src="plugins/revslider/public/assets/js/extensions/revolution.extension.layeranimation.min.js" async></script>
     <script type="text/javascript"
-        src="plugins/revslider/public/assets/js/extensions/revolution.extension.kenburn.min.js"></script>
+        src="plugins/revslider/public/assets/js/extensions/revolution.extension.kenburn.min.js" async></script>
     <script type="text/javascript"
-        src="plugins/revslider/public/assets/js/extensions/revolution.extension.navigation.min.js"></script>
+        src="plugins/revslider/public/assets/js/extensions/revolution.extension.navigation.min.js" async></script>
     <script type="text/javascript"
-        src="plugins/revslider/public/assets/js/extensions/revolution.extension.parallax.min.js"></script>
+        src="plugins/revslider/public/assets/js/extensions/revolution.extension.parallax.min.js" async></script>
     <script type="text/javascript"
-        src="plugins/revslider/public/assets/js/extensions/revolution.extension.actions.min.js"></script>
+        src="plugins/revslider/public/assets/js/extensions/revolution.extension.actions.min.js" async></script>
     <script type="text/javascript"
-        src="plugins/revslider/public/assets/js/extensions/revolution.extension.video.min.js"></script>
-        <script type='text/javascript' src='plugins/goodlayers-core/plugins/combine/script.js'></script>
+        src="plugins/revslider/public/assets/js/extensions/revolution.extension.video.min.js" async></script>
+        <script type='text/javascript' src='plugins/goodlayers-core/plugins/combine/script.js' async></script>
     <script type='text/javascript'>
         /* <![CDATA[ */
         var gdlr_core_pbf = {
@@ -36,36 +36,36 @@
         };
         /* ]]> */
     </script>
-    <script type='text/javascript' src='plugins/goodlayers-core/include/js/page-builder.js'></script>
-    <script type='text/javascript' src='js/jquery/ui/effect.min.js'></script>
+    <script type='text/javascript' src='plugins/goodlayers-core/include/js/page-builder.js' defer></script>
+    <script type='text/javascript' src='js/jquery/ui/effect.min.js' async></script>
     <!-- tween max js -->
     <!-- <script src="js/plugins/tween-max.min.js"></script> -->
     <!-- scroll magic js -->
-    <script src="js/plugins/scroll-magic.js"></script>
+    <script src="js/plugins/scroll-magic.js" async></script>
     <!-- scroll magic gsap plugin js -->
-    <script src="js/plugins/scroll-magic-gsap-plugin.js"></script>
+    <script src="js/plugins/scroll-magic-gsap-plugin.js" async></script>
     <!-- swiper js -->
-    <script src="js/plugins/swiper.min.js"></script>
+    <script src="js/plugins/swiper.min.js" defer></script>
     <!-- isotope js -->
-    <script src="js/plugins/isotope.min.js"></script>
+    <script src="js/plugins/isotope.min.js" async></script>
     <!-- fancybox js -->
-    <script src="js/plugins/fancybox.min.js"></script>
+    <script src="js/plugins/fancybox.min.js" defer></script>
     <!-- mapbox js -->
-    <script src="js/plugins/mapbox.min.js"></script>
+    <script src="js/plugins/mapbox.min.js" async></script>
     <!-- smooth scrollbar js -->
-    <script src="js/plugins/smooth-scrollbar.min.js"></script>
+    <script src="js/plugins/smooth-scrollbar.min.js" defer></script>
     <!-- overscroll js -->
-    <script src="js/plugins/overscroll.min.js"></script>
+    <script src="js/plugins/overscroll.min.js" defer></script>
     <!-- canvas js -->
-    <script src="js/plugins/canvas.js"></script>
+    <script src="js/plugins/canvas.js" async></script>
     <!-- parsley js -->
-    <script src="js/plugins/parsley.min.js"></script>
+    <script src="js/plugins/parsley.min.js" defer></script>
     <!-- main js -->
-    <script src="js/main.js"></script>
-    <script src="js/mailer.js"></script>
-    <script src="js/mailHelper.js"></script>
-    <script src="js/plugins.js"></script>
-    <script src="js/api.js"></script>
+    <script src="js/main.js" defer></script>
+    <script src="js/mailer.js" defer></script>
+    <script src="js/mailHelper.js" defer></script>
+    <script src="js/plugins.js" defer></script>
+    <script src="js/api.js" defer></script>
     </body>
 
 </html>
